8d1e0c986534d34c0e396b29adb0c27e5972e572
Oleksandr Verba <verba-tim@outlook.com>
2026-05-22 09:59:07 +0300

    Add the API functions.

---
+++ src/js/exported/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { ApiBaseURL, ApiParticles } from './constants.js';
+
+axios.defaults.baseURL = ApiBaseURL;
+const limit = 8;
+
+export async function getCategories() {
+  const response = await axios.get(ApiParticles[1]);
+  return response.data;
+}
+
+export async function getDesserts(page = 1, category) {
+  const parameters = {
+    params: {
+      page: page,
+      limit: limit,
+    },
+  };
+  if (category !== undefined) {
+    parameters.params.category = category;
+  }
+
+  const response = await axios.get(ApiParticles[0], parameters);
+  return response.data;
+}
+
+export async function getDessert(id) {
+  const response = await axios.get(`${ApiParticles[0]}/${id}`);
+  return response.data;
+}
+
+export async function getFeedbacks(limit = 10, page = 1) {
+  const parameters = {
+    params: {
+      page: page,
+      limit: limit,
+    },
+  };
+
+  const response = await axios.get(ApiParticles[3], parameters);
+  return response.data;
+}
+
+export async function postOrder({ name, phone, dessertId, comment }) {
+  const formData = {
+    name: name,
+    phone: phone,
+    dessertId: dessertId,
+    comment: comment,
+  };
+
+  const response = await axios.post(ApiParticles[2], formData);
+  return response.data;
+}
