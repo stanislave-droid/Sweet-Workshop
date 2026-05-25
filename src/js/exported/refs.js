f4a90cac6ccf960ff7da206076a3edd49fe904a4
Stanislave <stasdroner@gmail.com>
2026-05-25 03:05:27 +0300

    Add functional stuff to section

--- src/js/exported/refs.js
+++ src/js/exported/refs.js
@@ -7,3 +7,12 @@ export const sweetiesCategorySelect = document.querySelector(
 export const sweetiesCategoryLoader = document.querySelector(
   '.sweeties-category-loader'
 );
+export const sweetiesDessertsList = document.querySelector(
+  '.sweeties-desserts-list'
+);
+export const sweetiesLoadMoreBtn = document.querySelector(
+  '.sweeties-load-more-btn'
+);
+export const sweetiesDessertsLoader = document.querySelector(
+  '.sweeties-desserts-loader'
+);
--- src/js/partials/sweeties.js
+++ src/js/partials/sweeties.js
@@ -2,17 +2,30 @@ import {
   sweetiesCategoryList,
   sweetiesCategorySelect,
   sweetiesCategoryLoader,
+  sweetiesDessertsLoader,
+  sweetiesDessertsList,
 } from '/js/exported/refs';
 import {
   createMarkupForCategoryButtons,
   createMarkupForSelect,
 } from '/js/exported/render-functions';
 import { getCategories, getDesserts } from '/js/exported/api';
+import { onCategoryChange } from '/js/exported/handlers';
 import SlimSelect from 'slim-select';
 import 'slim-select/styles';
 
 const slimSelect = new SlimSelect({
   select: sweetiesCategorySelect,
+  events: {
+    afterChange: ([{ value }]) => {
+      if (value === 'Всі десерти') {
+        onCategoryChange();
+        return;
+      }
+
+      onCategoryChange(value);
+    },
+  },
   settings: {
     showSearch: false,
     openPosition: 'down',
@@ -30,17 +43,16 @@ getCategories()
       'beforeend',
       createMarkupForCategoryButtons(categories)
     );
+
+    sweetiesCategoryList.addEventListener('change', ({ target: { value } }) => {
+      onCategoryChange(value === '' ? undefined : value);
+    });
+    // sweetiesDessertsList.addEventListener("click", onCardButtonClick); For modal
   })
   .catch(error => {
     console.log(error);
   })
   .finally(() => {
     sweetiesCategoryLoader.hidden = true;
+    sweetiesDessertsLoader.hidden = true;
   });
-
-getDesserts()
-  .then(desserts => {})
-  .catch(error => {
-    console.log(error);
-  })
-  .finally(() => {});
