045e10afac26f2ca32a8203f6f70c653e92f43d1
Hyria Roman <hyrya.roman@gmail.com>
2026-05-25 15:55:52 +0300

    header/fix: js imported using wrong paths

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,5 +1,5 @@
-import { refs } from '../exported/refs.js';
-import { handleHeaderMenuClick, handleHeaderMenuEscape } from '../exported/handlers.js';
+import { refs } from '/js/exported/refs.js';
+import { handleHeaderMenuClick, handleHeaderMenuEscape } from '/js/exported/handlers.js';
 
 refs.headerMenu.addEventListener('click', handleHeaderMenuClick);
 document.addEventListener('keydown', handleHeaderMenuEscape);
