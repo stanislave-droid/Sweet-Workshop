cf24ed8c4f421306c70a9e67915e0700f04e53fa
Hyria Roman <hyrya.roman@gmail.com>
2026-05-26 18:31:48 +0300

    about-us/feat: add basic styles

--- src/js/partials/about-us.js
+++ src/js/partials/about-us.js
@@ -2,6 +2,8 @@ import { Swiper } from 'swiper';
 
 const swiper = new Swiper('.about-us__swiper', {
   enabled: false,
+  slidesPerView: 2,
+  spaceBetween: 24,
   breakpoints: {
     786: {
       enabled: true,
